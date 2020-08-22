e380091c02a5fe650356100b003080bab98d89ca
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2020-08-22 12:46:01 +0300

    Hw changes

--- closure-calculator/scripts.js
+++ closure-calculator/scripts.js
@@ -8,10 +8,13 @@ function createCalculator(baseValue) {
     };
 }
 
-const calc = createCalculator(5);
+let calc = createCalculator(5);
 
 console.log(calc.sum(10)); /// 15
 console.log(calc.mult(10)); // 150
 console.log(calc.sub(40)); // 110
 console.log(calc.div(10)); // 11
 console.log(calc.set(100)); //
+
+// Clear memory
+calc = null;
