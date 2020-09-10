546f288fd9ad69d46f7cf9173ba3d528b096f38f
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2020-09-10 10:21:21 +0300

    Lection

---
+++ lection/script.js
@@ -0,0 +1,41 @@
+// ООП
+
+// Наследование
+// Инкапсуляция
+// Полиморфизм
+
+// function Animal() {}
+
+// Animal.prototype.run = function () {
+//     console.log('animal is running', this.name);
+// };
+
+// function Cat(name) {
+//     this.name = name;
+// }
+
+// Cat.prototype = new Animal(); // {}
+
+// Cat.prototype.myau = function () {
+//     console.log('Nyau', this.name);
+// };
+
+// const bob = new Cat('bob');
+// const anim = new Animal();
+// // const bob1 = new Cat('bob1');
+// // const bob2 = new Cat('bob2');
+// // const bob3 = new Cat('bob3');
+
+
+
+function Stundent(//...){
+    // ....
+}
+
+const john = new students('John', [10, 9, 10, 9]);
+const bob = new students('sadf', [10, 9, 10, 9]);
+const asdf = new students('asdf', [10, 9, 10, 9]);
+const asdf = new students('aa', [10, 9, 10, 9]);
+
+console.log(john.avarageMark()) // John - 9.5
+ 
