de796625869054e0553d23d8ed4ab964765d93b6
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2020-10-18 21:27:02 +0300

    Lection

--- lection/script.js
+++ lection/script.js
@@ -1,3 +1,92 @@
-$(() => {
-    $('#date').datepicker();
-});
+// map, reduce, forEach, filter, find, flat, flatMap
+
+// class{
+//     constructor(){
+//         super()
+//     }
+//     sayHi(){
+//         super.sayHi()
+//     }
+// }
+
+// Promise
+
+//  let / const
+
+// function (){}
+// a => console.log()
+
+// function sayHi() {
+//     console.log(this);
+// }
+
+// sayHi();
+
+// const obj = {
+//     sayHello: sayHi,
+// };
+
+// obj.sayHello();
+
+// const anotherHello = () => console.log(this);
+
+// anotherHello();
+// const obj = {
+//     sayHello: anotherHello,
+// };
+
+// obj.sayHello();
+
+// function Person(name) {
+//     this.name = name;
+//     this.sayHi = function () {
+//         console.log(this.name);
+//     };
+//     this.sayHello = () => console.log(this.name);
+// }
+
+// const bob = new Person('Bob');
+// bob.sayHi();
+// bob.sayHello();
+
+// document.addEventListener('click', bob.sayHello);
+
+// 'ljsadhfkjshdf a'
+// ""
+
+// `asdlkdf ${a}
+
+// `
+
+// const obj = {
+//     name: 'Alex',
+//     age: 33,
+//     adress: { country: 'UA', city: 'Dnipro' },
+// };
+// // const { a: name, b, d:{f} } = obj;
+
+// const arr = [1, 2, 3, 4, 5];
+
+// // const [a, two, , t] = arr;
+
+// // console.log(arr);
+// // console.log(...arr);
+
+// // const arr1 = [...arr];
+
+// // const arr1 = [].concat(arr);
+
+// const obj1 = { ...obj, name: 'world' };
+
+// function sum(...args) {
+//     console.log(args);
+// }
+
+// sum(1);
+// sum(2, 3, 5, 6);
+
+// Set, Map
+
+const arr = [3, 3, 4, 6, 7, 3, 4];
+
+const s = new Set(arr);
