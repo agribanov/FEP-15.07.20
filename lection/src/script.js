ba7d39d936bd84d06fac45058020e4b4b49d1b72
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2020-11-04 20:37:50 +0200

    HW done

--- lection/src/script.js
+++ lection/src/script.js
@@ -1,3 +1,30 @@
-import { hello } from './another';
+const socket = new WebSocket('wss://fep-app.herokuapp.com/');
 
-hello();
+console.log(socket)
+
+
+socket.onopen = () => {
+    console.log('socket opened');
+    // socket.send('Hello world!')
+}
+
+socket.onmessage = (message) => {
+    console.log('message', JSON.parse(message.data))
+}
+
+socket.onclose = () => {
+    console.log('close')
+}
+
+socket.onerror = () => {
+    console.log('error')
+}
+
+document.getElementById('sendMsg').addEventListener('click', () => {
+    socket.send(JSON.stringify({
+        type: 'message',
+        payload: 'Alex!'
+    }));
+})
+
+
